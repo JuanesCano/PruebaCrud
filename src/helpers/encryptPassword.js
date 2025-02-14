0d25a29b1fdaa46564e585d6d14fcdb212e569f1
JuanesCano <juanestebancano.988@gmail.com>
2025-02-14 15:30:16 -0500

    [commit][juanes] feat: problem in bcrypt done

--- src/helpers/encryptPassword.js
+++ src/helpers/encryptPassword.js
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
 
-export const encryptPassword = async (password) => {
+export const encryptPassword = (password) => {
     try {
-        const salt = await bcrypt.genSaltSync(10);
-        const passwordEncriptada = await bcrypt.hashSync(password, salt);
+        const salt = bcrypt.genSaltSync(10);
+        const passwordEncriptada = bcrypt.hashSync(password, salt);
         return passwordEncriptada;
     } catch (error) {
         console.log("Error en el encriptPassword", error.message);
